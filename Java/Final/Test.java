3017a5b437e1fd453d882be5147f85b79698abd8
Tejas Bisen <tejas.bisen@remiges.tech>
2025-02-08 00:15:07 +0530

    learned Final Keyword in Java

---
+++ Java/Final/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(Utils.PI);
+        System.out.println(Utils.PI);
+        System.out.println(Utils.gravitityConstant);
+
+        System.out.println(Utils.NUM);
+    }
+}
---
+++ Java/Final/Utils.java
@@ -0,0 +1,13 @@
+public class Utils {
+
+    public static final double gravitityConstant = 9.81;
+    public static final double PI = 3.14;
+
+    public static final int NUM;
+
+    static {
+
+        NUM = 1262;
+    }
+
+}
